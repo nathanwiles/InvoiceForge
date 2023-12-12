262b24f027ce34d6e178724c9050f39a0ca2d7ad
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-12-12 15:51:09 -0500

    rework validation for appointmentRateCents and id

--- backend/dtos/appointment.js
+++ backend/dtos/appointment.js
@@ -4,21 +4,23 @@ moment().format();
 
 module.exports = yup.object().shape({
   invoiceId: yup.number().integer().positive().nullable().default(null),
-  clientId: yup.number().integer().positive().required(),
+  id: yup.number().integer().positive(),
+  clientId: yup.number().integer().positive(),
   date: yup.date().required(),
   startTime: yup.string().required(),
   endTime: yup.string()
     .required()
-    .test('is-after-start-time', "cannot-end-before-start", function(value) {
+    .test('end-is-after-start-time', "Cannot end before starting", function(value) {
       const { startTime } = this.parent;
-      return moment(value, 'HH:mm a').isSameOrAfter(moment(startTime, 'HH:mm a').add(1, 'hours'))
+      return moment(value, 'HH:mm a').isSameOrAfter(moment(startTime, 'HH:mm a').add(1, 'hours'));
     }),
   confirmedHours: yup.number().integer().positive().nullable().default(null),
   reviewed: yup.boolean().required().default(false),
   invoiced: yup.boolean().required().default(false),
-  appointmentRateCents: yup.number().integer().positive().nullable().default(null),
+  appointmentRateCents: yup.number().transform((value, originalValue) => { String(originalValue).trim() == "" ? null : value; }).integer().positive().nullable().default(null),
   notes: yup.string().nullable().default(null)
 })
-.test('id-clientId', 'must have either an id or a clientId', function(value) {
-  return !!(value.Id || value.clientId);
-});
+  .test('id-clientId', 'must have either an id or a clientId', function(value) {
+    console.log(value);
+    return !!(value.id || value.clientId);
+  });
