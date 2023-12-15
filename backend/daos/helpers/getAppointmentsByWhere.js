e97a749aea2c6e251351933fa276a5e7b2c8fbcd
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-12-15 12:28:52 -0500

    add documentation

--- backend/daos/helpers/getAppointmentsByWhere.js
+++ backend/daos/helpers/getAppointmentsByWhere.js
@@ -1,25 +1,36 @@
 const db = require('../../db/db');
-const humps = require('humps');
 
+/**
+ * Retrieves appointments from the database based on the provided conditions.
+ * If the condition includes a user_id, it retrieves appointments for the clients associated with that user.
+ * If the condition does not include a user_id, it retrieves all appointments that match the query.
+ * @param {Object} where - The conditions to filter the appointments.
+ * @returns {Promise<Array>} - A promise that resolves to an array of appointments.
+ */
 
-module.exports = async function (where) {
+module.exports = async function(where) {
+
   if (!!where.user_id) {
-    const {user_id, ...newWhere} = where;
+    // separate user_id from where object
+    const { user_id, ...newWhere } = where;
+    // retrieve client ids for user
     const clientIds = await db('clients_users').where({ user_id }).pluck('client_id');
-   // generate array of unique client ids
-   const uniqueClientIds = [...new Set(clientIds)];
-   // retrieve appointments for each client
-   const appointments = await Promise.all(uniqueClientIds.map(async (clientId) => {
-     const thisWhere = { ...newWhere, client_id: clientId };
-     return await db('appointments').where(thisWhere);
+    // generate array of unique client ids
+    const uniqueClientIds = [...new Set(clientIds)];
+    // retrieve appointments for each client
+    const appointments = await Promise.all(uniqueClientIds.map(async (clientId) => {
+      const thisWhere = { ...newWhere, client_id: clientId };
+      return await db('appointments').where(thisWhere);
 
-   }));
+    }));
 
+    // combine all appointments into one array
     const flattenedAppointments = appointments.reduce((acc, val) => acc.concat(val), []);
 
-  return flattenedAppointments
+    return flattenedAppointments;
   } else {
+    // return all appointments that match query
     const appointments = await db('appointments').where(where);
-    return humps.camelizeKeys(appointments);
+    return appointments;
   }
 };
